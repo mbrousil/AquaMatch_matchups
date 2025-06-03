42ce2eefe6cff063872390988cdc86843ca79baa
B Steele <B.Steele@colostate.edu>
2025-06-03 15:38:56 -0600

    add some architecture to check sites

--- _targets.R
+++ _targets.R
@@ -3,13 +3,7 @@
 # Load packages required to define the pipeline:
 library(targets)
 library(tarchetypes)
-library(reticulate)
 library(crew)
-
-# Set up python virtual environment ---------------------------------------
-
-tar_source("python/pySetup.R")
-
 
 # Set general configuration setting: -----------------------------
 
@@ -38,12 +32,6 @@ tar_option_set(
 
 # Define targets workflow -------------------------------------------------
 
-# Run the R scripts with custom functions:
-tar_source(c("4_compile_sites.R",
-             "5_determine_RS_visibility.R",
-             "6_siteSR_stack.R",
-             "7_qa_stack.R"))
-
 # and load the global functions
 tar_source("src/")
 
@@ -59,40 +47,6 @@ config_targets <- list(
     command = config::get(config = general_config),
     cue = tar_cue("always")
   ),
-  
-  # Check for Google Drive folder for siteSR output path, create it if it
-  # doesn't exist
-  tar_target(
-    name = p0_check_drive_parent_folder,
-    command = tryCatch({
-      drive_auth(p0_siteSR_config$google_email)
-      drive_ls(p0_siteSR_config$drive_project_folder)
-    }, error = function(e) {
-      drive_mkdir(str_sub(p0_siteSR_config$drive_project_folder, 1, -2))  
-    }),
-    packages = "googledrive",
-    cue = tar_cue("always")
-  ),
-  
-  # Check for targets subfolder, create if not present
-  tar_target(
-    name = p0_check_targets_drive,
-    command = {
-      p0_check_drive_parent_folder
-      tryCatch({
-        drive_auth(p0_siteSR_config$google_email)
-        drive_ls(paste0(p0_siteSR_config$drive_project_folder, "targets/"))
-      }, error = function(e) {
-        # if the outpath doesn't exist, create it along with a "stable" subfolder
-        drive_mkdir(name = "targets",
-                    path = p0_siteSR_config$drive_project_folder)
-      })
-      return(paste0(p0_siteSR_config$drive_project_folder, "targets/"))
-    },
-    packages = "googledrive",
-    cue = tar_cue("always")
-  ),
-  
   
   # Check for other pipelines -----------------------------------------------
   
@@ -111,19 +65,49 @@ config_targets <- list(
     cue = tar_cue("always")
   ),
   
+  # Grab location of the local {targets} WQP download pipeline OR error if
+  # the location doesn't exist yet
+  tar_target(
+    name = p0_AquaMatch_siteSR_WQP_directory,
+    command = if(dir.exists(p0_siteSR_config$siteSR_repo_directory)) {
+      p0_siteSR_config$siteSR_repo_directory
+    } else {
+      # Throw an error if the pipeline does not exist
+      stop("The siteSR download pipeline is not at the location specified in the 
+           config.yml file. Check the location specified as `siteSR_repo_directory`
+           in the config.yml file and rerun the pipeline.")
+    },
+    cue = tar_cue("always")
+  ),
+  
+  # Check for folder structure ----------------------------------------------
+  
+  tar_target(
+    name = p0_check_dir_structure,
+    command = if (!dir.exists("in/")) {dir.create("in/")},
+    cue = tar_cue("always")
+  ),
+  
   # Retrieve Drive IDs from linked repositories -------------------------------
+  
+  # site locations from siteSR pipeline
+  tar_file_read(
+    name = a_all_site_locs_Drive_id,
+    command = {
+      file.path(p0_AquaMatch_siteSR_WQP_directory,
+                "a_compile_sites/out/all_site_locations_drive_id.csv")
+    },
+    cue = tar_cue("always"),
+    read = read_csv(file = !!.x)
+  ),
   
   # Google Drive IDs of exported files from the download/harmonize pipeline
   
   tar_file_read(
     name = p3_chla_drive_ids,
     command = {
-      if(grepl("chla", p0_siteSR_config$parameters)) {
-        paste0(p0_AquaMatch_harmonize_WQP_directory,
-               "3_harmonize/out/chl_drive_ids.csv") 
-      } else {
-        NULL
-      }
+      file.path(p0_AquaMatch_harmonize_WQP_directory,
+             "3_harmonize/out/chl_drive_ids.csv") 
     },
     cue = tar_cue("always"),
     read = read_csv(file = !!.x)
@@ -132,12 +116,8 @@ config_targets <- list(
   tar_file_read(
     name = p3_sdd_drive_ids,
     command = {
-      if(grepl("sdd", p0_siteSR_config$parameters)) {
-        paste0(p0_AquaMatch_harmonize_WQP_directory,
-               "3_harmonize/out/sdd_drive_ids.csv") 
-      } else {
-        NULL
-      }
+      file.path(p0_AquaMatch_harmonize_WQP_directory,
+             "3_harmonize/out/sdd_drive_ids.csv") 
     },
     cue = tar_cue("always"),
     read = read_csv(file = !!.x)
@@ -146,12 +126,8 @@ config_targets <- list(
   tar_file_read(
     name = p3_doc_drive_ids,
     command = {
-      if(grepl("doc", p0_siteSR_config$parameters)) {
-        paste0(p0_AquaMatch_harmonize_WQP_directory,
-               "3_harmonize/out/doc_drive_ids.csv") 
-      } else {
-        NULL
-      }
+      file.path(p0_AquaMatch_harmonize_WQP_directory,
+             "3_harmonize/out/doc_drive_ids.csv") 
     },
     cue = tar_cue("always"),
     read = read_csv(file = !!.x)
@@ -160,34 +136,52 @@ config_targets <- list(
   tar_file_read(
     name = p3_tss_drive_ids,
     command = {
-      if(grepl("tss", p0_siteSR_config$parameters)) {
-        paste0(p0_AquaMatch_harmonize_WQP_directory,
-               "3_harmonize/out/tss_drive_ids.csv") 
-      } else {
-        NULL
-      }
+      file.path(p0_AquaMatch_harmonize_WQP_directory,
+             "3_harmonize/out/tss_drive_ids.csv") 
     },
     cue = tar_cue("always"),
     read = read_csv(file = !!.x)
   ),
   
-  # Google Drive IDs of exported files from the harmonize pipeline
+  # tar_file_read(
+  #   name = p3_tc_drive_ids,
+  #   command = {
+  #     file.path(p0_AquaMatch_harmonize_WQP_directory,
+  #            "3_harmonize/out/tc_drive_ids.csv") 
+  #   },
+  #   cue = tar_cue("always"),
+  #   read = read_csv(file = !!.x)
+  # ),
+  
   # Download files from Google Drive ----------------------------------------
+  
+  # siteSR site list
+  tar_target(
+    name = a_all_site_locations,
+    command = {
+      p0_check_dir_structure
+      retrieve_target(target = "a_all_site_locations",
+                      id_df = a_all_site_locs_Drive_id,
+                      local_folder = "in/",
+                      google_email = p0_siteSR_config$google_email,
+                      date_stamp = p0_siteSR_config$siteSR_site_version_date)
+    },
+    cue = tar_cue("always"),
+    packages = c("tidyverse", "googledrive")
+  ),
   
   # chlorophyll site list
   tar_target(
     name = p3_chla_harmonized_site_info,
     command = {
-      if (grepl("chla", p0_siteSR_config$parameter)) {
-        retrieve_target(target = "p3_chla_harmonized_site_info",
-                        id_df = p3_chla_drive_ids,
-                        local_folder = "4_compile_sites/in",
-                        google_email = p0_siteSR_config$google_email,
-                        date_stamp = p0_siteSR_config$chla_version_date) 
-      } else {
-        NULL
-      }
+      p0_check_dir_structure
+      retrieve_target(target = "p3_chla_harmonized_site_info",
+                      id_df = p3_chla_drive_ids,
+                      local_folder = "in/",
+                      google_email = p0_siteSR_config$google_email,
+                      date_stamp = p0_siteSR_config$chla_version_date) 
     },
+    cue = tar_cue("always"),
     packages = c("tidyverse", "googledrive")
   ),
   
@@ -195,16 +189,14 @@ config_targets <- list(
   tar_target(
     name = p3_sdd_harmonized_site_info,
     command = {
-      if (grepl("sdd", p0_siteSR_config$parameter)) {
-        retrieve_target(target = "p3_sdd_harmonized_site_info",
-                        id_df = p3_sdd_drive_ids,
-                        local_folder = "4_compile_sites/in",
-                        google_email = p0_siteSR_config$google_email,
-                        date_stamp = p0_siteSR_config$sdd_version_date)
-      } else {
-        NULL
-      }
+      p0_check_dir_structure
+      retrieve_target(target = "p3_sdd_harmonized_site_info",
+                      id_df = p3_sdd_drive_ids,
+                      local_folder = "in/",
+                      google_email = p0_siteSR_config$google_email,
+                      date_stamp = p0_siteSR_config$sdd_version_date)
     },
+    cue = tar_cue("always"),
     packages = c("tidyverse", "googledrive")
   ),
   
@@ -212,16 +204,14 @@ config_targets <- list(
   tar_target(
     name = p3_doc_harmonized_site_info,
     command = {
-      if (grepl("doc", p0_siteSR_config$parameter)) {
-        retrieve_target(target = "p3_doc_harmonized_site_info",
-                        id_df = p3_doc_drive_ids,
-                        local_folder = "4_compile_sites/in",
-                        google_email = p0_siteSR_config$google_email,
-                        date_stamp = p0_siteSR_config$doc_version_date)
-      } else {
-        NULL
-      }
+      p0_check_dir_structure
+      retrieve_target(target = "p3_doc_harmonized_site_info",
+                      id_df = p3_doc_drive_ids,
+                      local_folder = "in/",
+                      google_email = p0_siteSR_config$google_email,
+                      date_stamp = p0_siteSR_config$doc_version_date)
     },
+    cue = tar_cue("always"),
     packages = c("tidyverse", "googledrive")
   ),  
   
@@ -229,24 +219,32 @@ config_targets <- list(
   tar_target(
     name = p3_tss_harmonized_site_info,
     command = {
-      if (grepl("tss", p0_siteSR_config$parameter)) {
-        retrieve_target(target = "p3_tss_harmonized_site_info",
-                        id_df = p3_tss_drive_ids,
-                        local_folder = "4_compile_sites/in",
-                        google_email = p0_siteSR_config$google_email,
-                        date_stamp = p0_siteSR_config$tss_version_date)
-      } else {
-        NULL
-      }
+      p0_check_dir_structure
+      retrieve_target(target = "p3_tss_harmonized_site_info",
+                      id_df = p3_tss_drive_ids,
+                      local_folder = "in/",
+                      google_email = p0_siteSR_config$google_email,
+                      date_stamp = p0_siteSR_config$tss_version_date)
     },
+    cue = tar_cue("always"),
     packages = c("tidyverse", "googledrive")
   )
   
+  # # TC site list
+  # tar_target(
+  #   name = p3_tc_harmonized_site_info,
+  #   command = {
+  #     p0_check_dir_structure
+  #     retrieve_target(target = "p3_tc_harmonized_site_info",
+  #                     id_df = p3_tc_drive_ids,
+  #                     local_folder = "in/",
+  #                     google_email = p0_siteSR_config$google_email,
+  #                     date_stamp = p0_siteSR_config$tss_version_date)
+  #   },
+  #   packages = c("tidyverse", "googledrive")
+  # )
+  
 )
 
-# Full targets list
-c(config_targets,
-  p4_compile_sites,
-  p5_determine_RS_visibility,
-  p6_siteSR_stack,
-  p7_qa_stack)
+# # Full targets list
+# c(config_targets)
